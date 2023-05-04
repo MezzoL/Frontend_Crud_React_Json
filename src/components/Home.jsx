832c8423c9fc1a678e5a39f59e2256bd23ce2582
MezzoL <102746853+MezzoL@users.noreply.github.com>
2023-05-05 01:12:10 +0200

    better delete + search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 const fetchUsers = async () => {
   const { data } = await axios.get('http://localhost:3000/customers');
@@ -9,103 +9,174 @@ const fetchUsers = async () => {
 };
 
 function Home() {
-  const { data: users, isLoading, error } = useQuery(['users'], () => fetchUsers(), {
+  const { data: users, isLoading, error, refetch } = useQuery(['users'], () => fetchUsers(), {
     staleTime: 1000 * 60 * 10, // 10 minutes
   });
 
+  const deleteUserMutation = useMutation(
+    async (id) => {
+      await axios.delete(`http://localhost:3000/customers/${id}`);
+    },
+    {
+      onSuccess: () => {
+        refetch();
+      },
+    }
+  );
+
+  const [searchTerm, setSearchTerm] = useState('');
   const [displayCount, setDisplayCount] = useState(5);
 
   function deleteUser(id) {
-    axios.delete(`http://localhost:3000/customers/${id}`);
+    deleteUserMutation.mutate(id);
   }
 
   function showMore() {
     setDisplayCount(displayCount + 1);
   }
 
+  const filteredUsers = users
+    ? users.filter((user) => user.cname.toLowerCase().includes(searchTerm.toLowerCase()))
+    : [];
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-      <div className='w-full h-full justify-center items-center flex flex-col px-10 py-8 mt-8'>
-        <h1 className='text-3xl font-bold'>DATA TABLE</h1>
-        <div className='flex flex-col'>
-          <div className='overflow-x-auto mt-8 sm:-mx-6 items-center lg:-mx-8'>
-            <div className='py-4 inline-block min-w-full sm:px-6 lg:px-8'>
-              <div className='overflow-hidden'>
-                <table className='w-full text-center'>
-                  <thead className='border-b bg-gray-800'>
-                    <tr>
-                      <th scope='col' className='text-sm font-medium text-white px-6 py-4'>
-                        #
-                      </th>
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Name
-                      </th>
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Email
-                      </th>                      
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Address
-                      </th>
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Job Area
-                      </th>
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Workplace
-                      </th>
-                      <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
-                        Action
-                      </th>
+    <div className='w-full h-full justify-center items-center flex flex-col px-10 py-8 mt-8'>
+      <h1 className='text-3xl font-bold'>DATA TABLE</h1>
+      <input
+        type="text"
+        placeholder="Search users..."
+        className="mt-4 mb-8 px-3 py-2 border border-gray-300 rounded"
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      {filteredUsers.length === 0 && (
+        <div className="text-lg font-semibold mt-4">
+          No users found.
+        </div>
+      )}
+      <div className='flex flex-col'>
+        <div className='overflow-x-auto mt-8 sm:-mx-6 items-center lg:-mx-8'>
+          <div className='py-4 inline-block min-w-full sm:px-6 lg:px-8'>
+            <div className='overflow-hidden'>
+              <table className='w-full text-center'>
+                <thead className='border-b bg-gray-800'>
+                  <tr>
+                    <th scope='col' className='text-sm font-medium text-white px-6 py-4'>
+                      #
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Name
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Email
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Address
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Job Area
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Workplace
+                    </th>
+                    <th scope='col' className='text-sm font-lg text-white px-6 py-4'>
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className='border-black border-b-2'>
+                {searchTerm === '' ? users.slice(0, displayCount).map((data, index) => (
+                    <tr key={data.id} className='bg-white border-b-2 border-black'>
+                      <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 '>
+                        {index + 1}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.cname}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.email}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.address}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.jobarea}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.workplace}
+                      </td>
+                      <td className='text-sm flex justify-between  items-center text-gray-900 font-bold px-6 py-4 space-x-4 whitespace-nowrap'>
+                        <Link
+                          to={`/users/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          View
+                        </Link>
+                        <Link
+                          to={`/edit-user/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          Edit
+                        </Link>
+                        <Link
+                          onClick={() => deleteUser(data.id)} to={'#'} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          Delete
+                        </Link>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody className='border-black border-b-2'>
-                    {users.slice(0, displayCount).map((data, index) => (
-                      <tr key={data.id} className='bg-white border-b-2 border-black'>
-                        <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 '>
-                          {index + 1}
-                        </td>
-                        <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
-                          {data.cname}
-                        </td>
-                        <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
-                          {data.email}
-                        </td>                        
-                        <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
-                          {data.address}
-                        </td>
-                        <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
-                          {data.jobarea}
-                        </td>
-                        <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
-                          {data.workplace}
-                        </td>
-                        <td className='text-sm flex justify-between  items-center text-gray-900 font-bold px-6 py-4 space-x-4 whitespace-nowrap'>
-                          <Link
-                            to={`/users/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
-                            View
-                          </Link>
-                          <Link
-                            to={`/edit-user/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
-                            Edit
-                          </Link>
-                          <Link
-                            onClick={() => deleteUser(data.id)} to={'#'} className='bg-black text-white px-6 py-2 rounded-lg'>
-                            Delete
-                          </Link>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  )) : filteredUsers.map((data, index) => (
+                    <tr key={data.id} className='bg-white border-b-2 border-black'>
+                      <td className='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 '>
+                        {index + 1}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.cname}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.email}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.address}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.jobarea}
+                      </td>
+                      <td className='text-lg text-gray-900 font-semibold px-6 py-4 whitespace-nowrap'>
+                        {data.workplace}
+                      </td>
+                      <td className='text-sm flex justify-between  items-center text-gray-900 font-bold px-6 py-4 space-x-4 whitespace-nowrap'>
+                        <Link
+                          to={`/users/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          View
+                        </Link>
+                        <Link
+                          to={`/edit-user/${data.id}`} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          Edit
+                        </Link>
+                        <Link
+                          onClick={() => deleteUser(data.id)} to={'#'} className='bg-black text-white px-6 py-2 rounded-lg'>
+                          Delete
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
+      </div>
+      {filteredUsers.length === 0 && (
+        <div className="text-lg font-semibold mt-4">
+          No user found with the given search term.
+        </div>
+      )}
+      {!searchTerm && (
         <button
           onClick={showMore}
-          className='bg-black text-white px-6 py-2 rounded-lg mt-4'>Show More</button>
-      </div>
+          className='bg-black text-white px-6 py-2 rounded-lg mt-4'>
+          Show More
+        </button>
+      )}
+    </div>
   );
 }
 
